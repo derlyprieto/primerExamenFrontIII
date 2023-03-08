aff426b8ee8a815a02353d50759d1000f257cd55
Derly Prieto <derly@MacBook-Pro-de-Derly.local>
2023-03-08 17:47:01 -0500

    Creacion proyecto

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import './App.css'
+import Card from './Components/Card'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Card/>
+    </div>
+  )
+}
+
+export default App
